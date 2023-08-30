b4aaf1b04c947e088eb0d37f4986cdf1a2aa605e
Crowdbotics <team@crowdbotics.com>
2023-08-30 03:07:25 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fancy-haze-43143",
+  url: "https://fancy-haze-43143.botics.co",
+  api: "https://fancy-haze-43143.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
